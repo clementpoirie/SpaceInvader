1f1ed5cd2ba1ecdbdb57ea4d328fe3db47328e28
Lapalus Adrien <adri.lapalus@gmail.com>
2020-12-24 01:21:42 +0100

    V2

--- Space_InvaderA.py
+++ Space_InvaderA.py
@@ -14,66 +14,88 @@ Lien du git : https://github.com/clementpoirie/SpaceInvader.git
 ###################################################################################################################################################
 #                                                      Modules importés
 ###################################################################################################################################################
-from tkinter import Tk, Label, Button, Canvas, PhotoImage,ALL
+from tkinter import Tk, Label, Button, Canvas, PhotoImage , ALL
+from time import sleep
 
 ###################################################################################################################################################
 #                                                          Classes
 ###################################################################################################################################################
-
+global listeEN
 
 class CInterface:
     def __init__ (self):
         self.CreerFenetre()
         self.CreerToile()
+        self.CreerChamps()
+        self.CreerBoutons()
+        
+
     def CreerFenetre(self):
         "Creation de la fenetre"
         self.Fenetre = Tk()
         self.Fenetre.title("space invader")
         self.Fenetre.attributes('-fullscreen', True)
 
-        self.fullScreenState = False
-        self.Fenetre.bind("<F11>", self.toggleFullScreen)
-        self.Fenetre.bind("<Escape>", self.quitFullScreen)
+        self.etatFenetre = False
+        self.Fenetre.bind("<F11>", self.Remise_PleineEcran)
+        self.Fenetre.bind("<Escape>", self.Arreter_PleinEcran)
 
         # Le fichier .gif de l'image de fond est dans le répertoire "Gif_Autres", au même niveau que ce programme
         self.FichierGif_Fond = "Data/StarWars.png"
+        self.FichierPng_Vaisseau = "Data/Chasseur_Tie.png"
+
         self.ImageFond = PhotoImage(file=self.FichierGif_Fond)
+        self.ImageVaisseau = PhotoImage(file=self.FichierPng_Vaisseau)
+
         self.LargeurFenetre = self.ImageFond.width()
         self.HauteurFenetre = self.ImageFond.height()
 
-    def toggleFullScreen(self, event):
-        self.fullScreenState = not self.fullScreenState
-        self.Fenetre.attributes("-fullscreen", self.fullScreenState)
+    def Remise_PleineEcran(self, event):
+        self.etatFenetre = not self.etatFenetre
+        self.Fenetre.attributes("-fullscreen", self.etatFenetre)
 
-    def quitFullScreen(self, event):
-        self.fullScreenState = False
-        self.Fenetre.attributes("-fullscreen", self.fullScreenState)
+    def Arreter_PleinEcran(self, event):
+        self.etatFenetre = False
+        self.Fenetre.attributes("-fullscreen", self.etatFenetre)
 
     def CreerToile(self):
         "Creation de la Toile (Canevas)"
-        self.Toile = Canvas(self.Fenetre, width=self.LargeurFenetre,
-                            height=self.HauteurFenetre, background='white')
-        self.Toile.grid(row=0, column=0)
+        self.Toile = Canvas(self.Fenetre, width= 1350, height= 850, background='white')
+        self.Toile.grid(row=1, column=0 , columnspan = 3 , rowspan = 4 )
         self.Toile.delete(ALL)
-        self.Fond = self.Toile.create_image(
-            0, 0, image=self.ImageFond, anchor='nw')
+        self.Fond = self.Toile.create_image(0, 0, image=self.ImageFond, anchor='nw')
+
+
+    def CreerChamps(self):
+        "Creation des champs"
+        self.Score = Label(self.Fenetre, text="Score :" + '100', font='Arial 10', fg='black',anchor='w', borderwidth=0)
+        self.Score.grid(row=0, column=0, sticky='nw')
+
+        self.Vie = Label(self.Fenetre, text="Vie : :" + '3', font='Arial 10', fg='black',anchor='w', borderwidth=0)
+        self.Vie.grid(row=0, column=2, sticky='nw')
+    
+    def CreerBoutons(self):
+        self.Btn_Quitter = Button(self.Fenetre, text ='Quitter', width=15, command= self.Fenetre.destroy)
+        self.Btn_Quitter.grid(row=3, column=4, sticky='e', padx=15)
+
+        self.Btn_Recommencer = Button(self.Fenetre, text ='Nouvelle partie', width=15,command= lambda : creation_ennemie(self.Toile))
+        self.Btn_Recommencer.grid(row=2, column=4, sticky='e', padx=15)
 
     def Mainloop(self):
         self.Fenetre.mainloop()
 
-
 class Ennemie:
-    def __init__(self, canvas, ennemie):
-        self.Pcanvas=Canvas(canvas,height=100,width=100)
+    def __init__(self, canvas, ennemie,i):
+        self.Pcanvas=canvas
         self.Pfilename = PhotoImage(file="Data/X-wing_2.png")
-        self.X = 1
-        self.Y = 1
-        self.Pimage = self.Pcanvas.create_image(self.X,self.Y, image=self.Pfilename)
-        self.Pcanvas.place(anchor = "nw")
+        self.X = 150
+        self.Y = 50
+        self.Pimage = self.Pcanvas.create_image(self.X + i*100,self.Y, image=self.Pfilename)
         self.direction = 0
         self.limite = 0
         self.YMAX = 0
     def Mouvement(self):
+        print("slt")
         #Fonction permmettant le déplacement du vaisseau ennemie
         #Méthode : Si le vaisseau se trouve dans la fenêtre, il se déplace soit à gauche soit à droite suivant la valeur de direction
         #Ensuite si le vaiseau s'apprête à sortir de l'écran on initialise une valeur limite à 1 pour qu'il ne rentre plus dans le premier if
@@ -81,9 +103,9 @@ class Ennemie:
         #en Y du vaisseau puis lui rajoute une certaine valeur, par la suit cette valeur sera comparée avec la prochaine coordonnée du vaisseau.
         #Si la différence est égale à 0, on repasse la valeur limite à 0 puis on recommence le déplacement avec la direction changée
         if self.Getcoord()[1] < 850 :
-            if self.Getcoord()[0] > 0 and self.Getcoord()[0] < 100 and self.limite == 0 : #légerement inférieur a la taille du canavs
+            if self.Getcoord()[0] > 0 and self.Getcoord()[0] < 1000 and self.limite == 0 : #légerement inférieur a la taille du canavs
                 self.Direction(self.direction)
-            elif self.Getcoord()[0] >= 100 and self.limite == 0:
+            elif self.Getcoord()[0] >= 1000 and self.limite == 0:
                 self.direction = 1
                 self.limite = 1
                 self.X = 99
@@ -99,23 +121,39 @@ class Ennemie:
             elif abs(self.Getcoord()[1] - self.YMAX) <= 0 and self.limite == 1:
                 self.limite = 0
                 self.Direction(self.direction)
+        else :
+            self.Pcanvas.destroy        
         self.Pcanvas.place(anchor = "nw")        
-        fenetre.Fenetre.after(10,self.Mouvement())
+        fenetre.Fenetre.after(10,self.Mouvement)
     def Getcoord(self):
         # fonction pour récuperer les coordonnées du Vaisseau
         return self.Pcanvas.coords(self.Pimage)
 
     def Direction(self, sens):
         if self.direction == 0:
-            self.Pcanvas.move(self.Pimage, 2, 0)
+            self.Pcanvas.move(self.Pimage, 10, 0)
         elif self.direction == 1:
-            self.Pcanvas.move(self.Pimage, -2, 0)
+            self.Pcanvas.move(self.Pimage, -10, 0)
+
+
+
+
+
+def creation_ennemie(Toile):
+    listeEN = []
+    for  i in range(10):
+        listeEN.append(Ennemie(Toile,1,i)) 
+    fenetre.Fenetre.after(10,a)  
+
+def Debut_Partie(listeEN):
+    print("SLT")
+    for i in range(len(listeEN)):
+        listeEN[i].Mouvement
 ###################################################################################################################################################
 #                                                            Main
 ###################################################################################################################################################
 
-
+listeEN = []
 fenetre = CInterface()
-ennemie = Ennemie(fenetre.Toile,1)
-ennemie.Mouvement()
+fenetre.Toile.bind('x',Debut_Partie(listeEN))
 fenetre.Mainloop()
