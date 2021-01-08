bbd2e304064800e5b80dd47945726153a32776af
Clement Poirie <clement.poirie@orange.fr>
2021-01-08 19:06:04 +0100

    ffff

--- SpaceInvaderC.py
+++ SpaceInvaderC.py
@@ -14,13 +14,12 @@ Lien du git : https://github.com/clementpoirie/SpaceInvader.git
 ###################################################################################################################################################
 #                                                      Modules importés
 ###################################################################################################################################################
-from tkinter import Tk, Label, Button, Canvas, PhotoImage , ALL
+from tkinter import Tk, Label, Button, Canvas, PhotoImage , ALL 
 from time import sleep
 
 ###################################################################################################################################################
 #                                                          Classes
 ###################################################################################################################################################
-
 
 class CInterface:
     def __init__ (self):
@@ -28,6 +27,9 @@ class CInterface:
         self.CreerToile()
         self.CreerChamps()
         self.CreerBoutons()
+        
+        
+       
         
 
     def CreerFenetre(self):
@@ -40,9 +42,11 @@ class CInterface:
         self.Fenetre.bind("<F11>", self.Remise_PleineEcran)
         self.Fenetre.bind("<Escape>", self.Arreter_PleinEcran)
 
+        
+
         # Le fichier .gif de l'image de fond est dans le répertoire "Gif_Autres", au même niveau que ce programme
         self.FichierGif_Fond = "Data/StarWars.png"
-        self.FichierPng_Vaisseau = "Data/Chasseur_Tie.png"
+        self.FichierPng_Vaisseau = "Data/X-wing_2.png"
 
         self.ImageFond = PhotoImage(file=self.FichierGif_Fond)
         self.ImageVaisseau = PhotoImage(file=self.FichierPng_Vaisseau)
@@ -65,7 +69,7 @@ class CInterface:
         self.Toile.delete(ALL)
         self.Fond = self.Toile.create_image(0, 0, image=self.ImageFond, anchor='nw')
 
-
+        
     def CreerChamps(self):
         "Creation des champs"
         self.Score = Label(self.Fenetre, text="Score :" + '100', font='Arial 10', fg='black',anchor='w', borderwidth=0)
@@ -75,24 +79,26 @@ class CInterface:
         self.Vie.grid(row=0, column=2, sticky='nw')
     
     def CreerBoutons(self):
-        self.Btn_Quitter = Button(self.Fenetre, text ='Quitter', width=15, command=self.Fenetre.destroy)
+        self.Btn_Quitter = Button(self.Fenetre, text ='Quitter', width=15, command= self.Fenetre.destroy)
         self.Btn_Quitter.grid(row=3, column=4, sticky='e', padx=15)
 
-        self.Btn_Recommencer = Button(self.Fenetre, text ='Nouvelle partie', width=15, command=self.Fenetre.destroy)
+        self.Btn_Recommencer = Button(self.Fenetre, text ='Nouvelle partie', width=15,command= lambda : nouvelle_partie(self.Toile))
         self.Btn_Recommencer.grid(row=2, column=4, sticky='e', padx=15)
 
+   
     def Mainloop(self):
         self.Fenetre.mainloop()
 
-
 class Ennemie:
-    def __init__(self, canvas, ennemie):
-        self.Pcanvas = canvas
-        self.Pfilename = PhotoImage(file=ennemie)
-        self.Pimage = self.Pcanvas.create_image(
-            -100, 255, anchor='NW', image=self.Pfilename)
+    def __init__(self, canvas, ennemie,i):
+        self.Pcanvas=canvas
+        self.Pfilename = PhotoImage(file="Data/X-wing_2.png")
+        self.X = 150
+        self.Y = 50
+        self.Pimage = self.Pcanvas.create_image(self.X + i*100,self.Y, image=self.Pfilename)
         self.direction = 0
         self.limite = 0
+        self.YMAX = 0
 
     def Mouvement(self):
         #Fonction permmettant le déplacement du vaisseau ennemie
@@ -101,20 +107,28 @@ class Ennemie:
         #puis on initalise une autre valeur qui servira de limite quant au déplacement du vaisseau vers le bas; cette dernière valeur récupère la coordonnée
         #en Y du vaisseau puis lui rajoute une certaine valeur, par la suit cette valeur sera comparée avec la prochaine coordonnée du vaisseau.
         #Si la différence est égale à 0, on repasse la valeur limite à 0 puis on recommence le déplacement avec la direction changée
-        if self.Pcanvas.Getcoord()[0] > 0 and self.Pcanvas.Getcoord()[0] < 100 and self.limite == 0 : #légerement inférieur a la taille du canavs
-            self.Direction(self.direction)
-        if self.Pcanvas.Getcoord()[0] >= 100 and self.limite == 0:
-            self.direction = 1
-            self.limite = 1
-            YMAX = self.Pcanvas.Getcoord()[1]+20
-        if self.Pcanvas.Getcoord()[0] <= 0 and self.limite == 0:
-            self.direction = 0
-            self.limite = 1
-            YMAX = self.Pcanvas.Getcoord()[1]+20
-        if abs(self.Pcanvas.Getcoord()[1] - YMAX) > 0 and self.limite == 1:
-            self.Pcanvas.move(self.Pimage, 0, 2)
-        if abs(self.Pcanvas.Getcoord()[1] - YMAX) <= 0 and self.limite == 1:
-            self.limite = 0
+        if self.Getcoord()[1] < 850 :
+            if self.Getcoord()[0] > 5 and self.Getcoord()[0] < 1345 and self.limite == 0 : #légerement inférieur a la taille du canavs
+                self.Direction(self.direction)
+            elif self.Getcoord()[0] >= 1345 and self.limite == 0:
+                self.direction = 1
+                self.limite = 1
+                self.X = 99
+                self.YMAX = self.Getcoord()[1]+20
+            elif self.Getcoord()[0] <= 5 and self.limite == 0:
+                self.direction = 0
+                self.limite = 1
+                self.X = 1
+                self.YMAX = self.Getcoord()[1]+20    
+            elif abs(self.Getcoord()[1] - self.YMAX) > 0 and self.limite == 1:
+                print(abs(self.Getcoord()[1] - self.YMAX))
+                self.Pcanvas.move(self.Pimage, 0, 20)
+            elif abs(self.Getcoord()[1] - self.YMAX) <= 0 and self.limite == 1:
+                self.limite = 0
+                self.Direction(self.direction)
+        else :
+            self.Pimage.destroy             
+        fenetre.Fenetre.after(10,self.Mouvement)
 
     def Getcoord(self):
         # fonction pour récuperer les coordonnées du Vaisseau
@@ -122,13 +136,60 @@ class Ennemie:
 
     def Direction(self, sens):
         if self.direction == 0:
-            self.Pcanvas.move(self.Pimage, 2, 0)
+            self.Pcanvas.move(self.Pimage, 10, 0)
         elif self.direction == 1:
-            self.Pcanvas.move(self.Pimage, -2, 0)
+            self.Pcanvas.move(self.Pimage, -10, 0)
+
+
+class Camis:
+    def __init__(self , canvas ):
+        self.Pcanvas=canvas
+        #self.ImageFond = "Data/X-wing_2.png"
+        self.vaisseau = Canvas(self.Pcanvas, width = 20 , height = 20 , background = 'white') 
+        #self.Fond = self.vaisseau.create_image(0, 0, image=self.ImageFond, anchor='nw')
+        self.vaisseau.place(x = 1350 / 2 , y = 850 - 100)  
+
+        self.Pcanvas.bind("<Key-Left>", self.mouvementG)
+        self.Pcanvas.bind("<Key-Right>", self.mouvementD)
+        
+
+         
+    def mouvementG(self , event ):
+        print('gauche')
+        self.Pcanvas.move(self.vaisseau, -10, 0)
+
+    def mouvementD (self , event):
+        print('droite')
+        self.Pcanvas.move(self.vaisseau, 10, 0)
+       
+
+def mouvementAllié(amis):
+    print('coucou')
+    amis.mouvement()
+
+
+def nouvelle_partie (Toile):
+    amis = Camis(Toile)
+    creation_ennemie(Toile)
+    return(amis)
+  
+def creation_ennemie(Toile):
+    global listeEN
+    listeEN = []
+    for  i in range(10):
+        listeEN.append(Ennemie(Toile,1,i)) 
+    fenetre.Fenetre.after(10,a) 
+    return listeEN 
+
+def Debut_Partie(event):
+    print(len(listeEN))
+    for i in range(len(listeEN)):
+        listeEN[i].Mouvement()
+
 ###################################################################################################################################################
 #                                                            Main
 ###################################################################################################################################################
 
-
 fenetre = CInterface()
+fenetre.Fenetre.bind('x',Debut_Partie)
 fenetre.Mainloop()
